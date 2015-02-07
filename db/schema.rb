2e4b157ee152ee31fe22883eb5ab13df84803ba6
Rudolf Jantos <rudolf.jantos@gmail.com>
2015-02-07 09:51:55 +0100

    Added Tasks scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150202211954) do
+ActiveRecord::Schema.define(version: 20150207084244) do
+
+  create_table "tasks", force: true do |t|
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
